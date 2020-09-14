5f9ee6d00fb0779f0b1148e9daa176d0ae53094f
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2020-09-14 15:59:43 -0400

    completed

---
+++ src/context/FilterContext.js
@@ -0,0 +1,15 @@
+import React, { useState, createContext } from "react"
+export const FilterContext = createContext()
+
+/* le component-provider qui embracera la partie de notre app où on utilise ce context */
+
+const FilterContextProvider = ({ children }) => {
+  const [filter, setFilter] = useState("all")
+  return (
+    <FilterContext.Provider value={{ filter, setFilter }}>
+      {children}
+    </FilterContext.Provider>
+  )
+}
+
+export default FilterContextProvider
